2d7a2eaac5b858e4bd8896df227e28535020afc3
Jiang Chen <criver@gmail.com>
2015-07-14 22:05:07 -0700

    More packages

--- setup.sh
+++ setup.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
-#install brew
-# ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-# sudo chown root /usr/local/bin/brew
-echo ${OSTYPE}
+
 
 if [[ "${OSTYPE}" =~ "darwin"* ]];
 then
+    # ---  OSX ---- #
+    # ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    # sudo chown root /usr/local/bin/brew
     INSTALL="sudo brew install"
     brew tap homebrew/science
 else
     INSTALL="sudo apt-get"
 fi
 
-packages=(liblas python ruby scalar emacs boost leveldb gflags opencv hdf5 gcc glob protobuf git curl openssl gradle snappy ag)
+packages=(liblas python ruby scalar emacs boost leveldb gflags opencv hdf5 gcc glob protobuf git curl openssl gradle snappy ag lmdb re2)
 
 for package in ${packages[*]}; do
     ${INSTALL} $package
 done
-exit
 
-python_packages=(numpy pandas nltk gensim scipy stemming ipython "ipython[notebook]" protobuf)
+python_packages=(numpy pandas nltk gensim scipy stemming ipython "ipython[notebook]" protobuf requests nose scikit-learn scikit-image mock re2)
 for package in ${python_packages[*]}; do
     sudo pip install ${package}
 done
