d0cdb41335408d1e0357392167bfb41f60f317d1
LDAP/jiang_chen <jiang_chen@i-cb80d318.inst.aws.airbnb.com>
2015-07-15 23:09:30 +0000

    Added a few packages

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-packages=(liblas python ruby scalar emacs boost leveldb gflags opencv hdf5 gcc glob protobuf git curl openssl gradle snappy ag lmdb re2)
+packages=(liblas python python-pip ruby scalar emacs emacs24 emacs24-el emacs24-common-non-dfsg boost leveldb gflags opencv hdf5 gcc glob protobuf git curl openssl gradle snappy ag lmdb re2)
 if [[ "${OSTYPE}" =~ "darwin"* ]];
 then
     # ---  OSX ---- #
@@ -10,6 +10,11 @@ then
     brew tap homebrew/science
 else
     INSTALL="sudo apt-get install -y"
+    sudo add-apt-repository ppa:cassou/emacs
+    sudo apt-get update
+    sudo apt-get builddep matplotlib
+    sudo apt-get builddep numpy
+    sudo apt-get builddep scipy
     packages+=(gfortran libopenblas-dev liblapack-dev)
 fi
 
@@ -17,7 +22,7 @@ for package in ${packages[*]}; do
     ${INSTALL} $package
 done
 
-python_packages=(numpy pandas nltk gensim scipy stemming ipython "ipython[notebook]" protobuf requests nose scikit-learn scikit-image mock re2 jinjia2 jsonschema functools32)
+python_packages=(numpy pandas nltk gensim scipy stemming ipython=3.2.1 "ipython[notebook]" protobuf requests nose scikit-learn scikit-image mock re2 jinjia2 jsonschema functools32 pyzmq)
 for package in ${python_packages[*]}; do
     sudo pip install ${package}
 done
