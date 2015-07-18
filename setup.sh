8881cd2d55cacf16aaf70b0163f7a6a6407db168
LDAP/jiang_chen <jiang_chen@i-7262e2da.inst.aws.airbnb.com>
2015-07-18 15:58:09 +0000

    Move the packages to a requirements.txt file.

--- setup.sh
+++ setup.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
-packages=(liblas python python-pip ruby scalar emacs emacs24 emacs24-el emacs24-common-non-dfsg boost leveldb gflags opencv hdf5 gcc glob protobuf git curl openssl gradle snappy ag lmdb re2)
 if [[ "${OSTYPE}" =~ "darwin"* ]];
 then
     # ---  OSX ---- #
     # ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     # sudo chown root /usr/local/bin/brew
-    INSTALL="sudo brew install"
     brew tap homebrew/science
+    cat osx/requirements.txt | xargs sudo brew install
 else
-    INSTALL="sudo apt-get install -y"
+    # --- Ubuntu -- #
     sudo add-apt-repository ppa:cassou/emacs
-    sudo apt-get update
+    cat ubuntu/requirements.txt | xargs sudo aptitude install -y
     sudo easy_install -U distribute
-    packages+=(gfortran libopenblas-dev liblapack-dev libpng-dev libfreetype6-dev)
 fi
 
-for package in ${packages[*]}; do
-    ${INSTALL} $package
-done
-
+# --- Python --- #
 pip install -r python/requirements.txt
-python_packages=(numpy pandas nltk gensim scipy stemming ipython==3.2.1 "ipython[notebook]" protobuf requests nose scikit-learn scikit-image mock re2 jinja2 jsonschema functools32 pyzmq tornado matplotlib)
